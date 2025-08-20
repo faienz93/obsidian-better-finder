d2744454fc08556ca81b64dcc6657fb988587eb4
faienz93 <afaienza93@gmail.com>
2025-08-20 18:35:43 +0200

    feat: refactor code and remove unused component

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin } from 'obsidian';
-import AdvancedSearchModal from './src/BetterFinderModal'
+import { MarkdownView, Plugin } from 'obsidian';
+import FinderModal from './src/FinderModal'
 import { BetterFinderView, VIEW_TYPE_BETTERFINDER } from "./src/BetterFinderView";
-import FinderSetting from './src/Setting'
-
-// Remember to rename these classes and interfaces!
+import FinderSetting from './src/FinderSetting'
 
 interface ObsidianBetterFinderSettings {
   mySetting: string;
@@ -24,66 +22,13 @@ export default class ObsidianBetterFinder extends Plugin {
     );
     await this.loadSettings();
 
-    // This creates an icon in the left ribbon.
-    const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-      // Called when the user clicks the icon.
-      new Notice('This is a notice!');
-    });
-    // Perform additional things with the ribbon
-    ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-    const statusBarItemEl = this.addStatusBarItem();
-    statusBarItemEl.setText('Status Bar Text');
-
-    // This adds a simple command that can be triggered anywhere
     this.addCommand({
       id: 'open-sample-modal-simple',
       name: 'Open sample modal (simple)',
       callback: () => {
-        new SampleModal(this.app).open();
+        new FinderModal(this.app).open();
       }
     });
-    // This adds an editor command that can perform some operation on the current editor instance
-    this.addCommand({
-      id: 'sample-editor-command',
-      name: 'Sample editor command',
-      editorCallback: (editor: Editor, view: MarkdownView) => {
-        console.log(editor.getSelection());
-        editor.replaceSelection('Sample Editor Command');
-      }
-    });
-    // This adds a complex command that can check whether the current state of the app allows execution of the command
-    this.addCommand({
-      id: 'open-sample-modal-complex',
-      name: 'Open sample modal (complex)',
-      checkCallback: (checking: boolean) => {
-        // Conditions to check
-        const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-        if (markdownView) {
-          // If checking is true, we're simply "checking" if the command can be run.
-          // If checking is false, then we want to actually perform the operation.
-          if (!checking) {
-            new SampleModal(this.app).open();
-          }
-
-          // This command will only show up in Command Palette when the check function returns true
-          return true;
-        }
-      }
-    });
-
-
-
-
-    this.addCommand({
-      id: "open-advanced-search",
-      name: "Apri Ricerca Avanzata",
-      callback: () => {
-        new AdvancedSearchModal(this.app).open();
-      }
-    });
-
 
     this.addCommand({
       id: 'open-better-finder',
@@ -102,9 +47,7 @@ export default class ObsidianBetterFinder extends Plugin {
       console.log('click', evt);
     });
 
-    this.addRibbonIcon('dice', 'Greet', () => {
-      new Notice('Hello, world!');
-    });
+
 
     // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
     this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -136,20 +79,6 @@ export default class ObsidianBetterFinder extends Plugin {
 
 }
 
-class SampleModal extends Modal {
-  constructor(app: App) {
-    super(app);
-  }
 
-  onOpen() {
-    const { contentEl } = this;
-    contentEl.setText('Woah!');
-  }
-
-  onClose() {
-    const { contentEl } = this;
-    contentEl.empty();
-  }
-}
 
 
