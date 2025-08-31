40c516b932c9e71ce53af34716bfe87ce13ae3b9
faienz93 <afaienza93@gmail.com>
2025-08-31 14:24:26 +0200

    feat: added layout for searched tags

--- src/FinderModal.ts
+++ src/FinderModal.ts
@@ -43,7 +43,7 @@ class FinderModal extends SuggestModal<TFile> {
   renderSuggestion(file: TFile, el: HTMLElement) {
     el.createEl('div', { text: file.name });
     // el.createEl('small', { text: file.basename });
-    el.createEl('small', { text: file.extension });
+    // el.createEl('small', { text: file.extension });
     el.createEl('small', { text: new Date(file.stat.ctime).toLocaleDateString() });
 
     const fileCache = this.app.metadataCache.getFileCache(file);
@@ -64,7 +64,7 @@ class FinderModal extends SuggestModal<TFile> {
         fileTags.forEach(tag => {
           const tagEl = tagsContainer.createSpan({ text: tag });
           tagEl.style.background = 'var(--tag-background, #484848)';
-          tagEl.style.color = 'var(--tag-color, #a855f7)';
+          // tagEl.style.color = 'var(--tag-color, #a855f7)';
           tagEl.style.padding = '2px 6px';
           tagEl.style.borderRadius = '4px';
           tagEl.style.fontSize = '11px';
